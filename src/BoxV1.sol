6a40108fde19c0056400a63bf7859fc6d0c2858a
FalseGenius <billionairejackal@gmail.com>
2024-04-06 15:59:26 +0500

    natspec added

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,12 +1,36 @@
 // SPDX-License-Identifier: MIT
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 
 pragma solidity ^0.8.18;
 
-contract BoxV1 {
+/**
+ * @notice Proxy contracts do not have constructors.
+ * Constructors add storage to implementation. Initializers are esentially constructors
+ * for proxy.
+ */
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable, ERC20Upgradeable{
+
+    error BoxV1__YouAreNotTheOwner();
+
     uint256 internal number;
+
+        /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /**
+     * @dev This is upgradeable/initializable function. It sets owner to msg.sender.
+     */
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -16,5 +40,5 @@ contract BoxV1 {
         return 1;
     }
 
-    function _authorizeUpgrade(address newImplementation) internal virtual {}
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
