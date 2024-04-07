abfe0d2e337e36d79a51d818916ebc986e126164
FalseGenius <billionairejackal@gmail.com>
2024-04-07 13:19:38 +0500

    DevOpsTools added

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract UpgradeBox is Script {
 
+    function run() external returns (address) {
+        address getMostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BoxV1", block.chainid);
+        
+    }
 }
