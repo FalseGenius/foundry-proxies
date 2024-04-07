1c2c0a96667153f76270aabd7a8af92bc7036363
FalseGenius <billionairejackal@gmail.com>
2024-04-07 16:37:37 +0500

    BoxV2->setNumber update

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -7,14 +7,16 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber() external {}
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
     }
 
     function version() external pure returns (uint256) {
-        return 1;
+        return 2;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override {}
